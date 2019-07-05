5b2dd33d8c35b968f8ddd85935752bf3868adc04
Rakhmetzhanov Meirkhan <meirkhan.rakhmetzhanov@orange.com>
2019-07-05 16:07:59 +0200

    added class for String fields

--- src/main/java/com/meirkhan/kafka/MySchemas.java
+++ src/main/java/com/meirkhan/kafka/MySchemas.java
@@ -1,4 +1,6 @@
 package com.meirkhan.kafka;
 
 public class MySchemas {
+    public static final String DATABASE_NAME_FIELD = "database";
+    public static final String COLLECTION_FIELD = "collection";
 }
